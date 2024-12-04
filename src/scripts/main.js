3fd3e50006a491a112f7d4b44b4bd335fa02849e
Marco Antonio <marcoarchaga10@gmail.com>
2024-12-04 07:24:59 -0600

    cambio en el codigo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,19 +5,19 @@ import {
     reservar,
     mostrarNumeros,
     configurarBotonReservar
-} from './reservar'; // Reemplaza 'nombreArchivo' con el nombre real del archivo donde exportaste las funciones
+} from './reservar'; 
 
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM completamente cargado');
 
-    // Inicializar el menú hamburguesa
+    
     menuHamburguesa();
 
-    // Inicializar galería
+    
     iniciarGallery();
 
-    // Configurar eventos específicos
+    
     consultarDisponibilidad();
     configurarBotonReservar();
     reservar();
